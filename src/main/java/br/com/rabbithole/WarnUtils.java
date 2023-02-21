d9c838765614365f7f98337362e5d784e37f0316
FelipeSz19 <feiperos.simao@gmail.com>
2023-02-20 22:32:59 -0300

    misc

--- src/main/java/br/com/rabbithole/WarnUtils.java
+++ src/main/java/br/com/rabbithole/WarnUtils.java
@@ -5,6 +5,9 @@ import br.com.rabbithole.core.WarnExecutor;
 public class WarnUtils {
     private static WarnExecutor warn;
 
+    public WarnUtils() {
+    }
+
     public void warnInitializer(String prefix) {
         warn = new WarnExecutor(prefix);
         warn.sendWarn("<green>Instância de WarnLib iniciada em Projeto: "+prefix);
