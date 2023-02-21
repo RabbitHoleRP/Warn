c8cfd7e6eaebb816c989aa9712408bd57e6a7bec
FelipeSz19 <feiperos.simao@gmail.com>
2023-02-20 22:22:22 -0300

    fix: static instance error

--- src/main/java/br/com/rabbithole/WarnUtils.java
+++ src/main/java/br/com/rabbithole/WarnUtils.java
@@ -5,7 +5,7 @@ import br.com.rabbithole.core.WarnExecutor;
 public class WarnUtils {
     private static WarnExecutor warn;
 
-    public static void warnInitializer(String prefix) {
+    public void warnInitializer(String prefix) {
         warn = new WarnExecutor(prefix);
         warn.sendWarn("<green>Instância de WarnLib iniciada em Projeto: "+prefix);
     }
